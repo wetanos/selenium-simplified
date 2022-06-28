286d59415ccf07c2a7698d0a96ea29bb64080e17
Kirill Vasilenko <wetanos@gmail.com>
2022-06-28 17:40:24 +0300

    feat: test selenium

---
+++ src/test/java/TestSelenium.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class TestSelenium {
+    @Test
+    public void startWebDriver(){
+        System.setProperty("webdriver.chrome.driver", "/Users/kirillvasilenko/Documents/Programs/chromedriver");
+
+        //WebDriver driver = new FirefoxDriver();
+        WebDriver driver = new ChromeDriver();
+        driver.navigate().to("http://seleniumsimplified.com");
+        Assert.assertTrue("title should start differently",
+                driver.getTitle().startsWith("Selenium Simplified"));
+        driver.close();
+        driver.quit();
+    }
+}
+
